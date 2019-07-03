b1cd573fcff8afefe0b9b63f227db9399d7704ff
Dan Hett <hellodanhett@gmail.com>
2019-07-03 12:01:19 +0100

    Revert URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.post('/voice', (request, response) => {
   const twiml = new VoiceResponse();
   twiml.play({
       loop: 1
-  }, 'https://www.dropbox.com/s/obu1f2830ffmf41/HOTLINE_Bells.mp3?dl=1');
+  }, 'http://rave.danhett.com/yes.mp3');
 
   response.type('text/xml');
   response.send(twiml.toString());
